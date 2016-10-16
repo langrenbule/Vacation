7cc6d6e42aef9c5cac0ff7b831f34daac30416fc
langrenbule <546024423@qq.com>
2016-10-16 17:56:37 +0800

    (*)对Bomb的NewIM模块进行了移植--主页

--- app/src/main/java/com/deity/vacation/MainActivity.java
+++ app/src/main/java/com/deity/vacation/MainActivity.java
@@ -1,139 +1,202 @@
 package com.deity.vacation;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import com.deity.vacation.fragment.CheeseListFragment;
+import com.deity.vacation.base.BaseActivity;
+import com.deity.vacation.dao.NewFriendManager;
+import com.deity.vacation.entity.User;
+import com.deity.vacation.event.RefreshEvent;
+import com.deity.vacation.fragment.ContactFragment;
+import com.deity.vacation.fragment.ConversationFragment;
+import com.deity.vacation.fragment.SetFragment;
+import com.deity.vacation.utils.IMMLeaks;
+import com.orhanobut.logger.Logger;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import butterknife.Bind;
+import cn.bmob.newim.BmobIM;
+import cn.bmob.newim.core.ConnectionStatus;
+import cn.bmob.newim.event.MessageEvent;
+import cn.bmob.newim.event.OfflineMessageEvent;
+import cn.bmob.newim.listener.ConnectListener;
+import cn.bmob.newim.listener.ConnectStatusChangeListener;
+import cn.bmob.newim.listener.ObseverListener;
+import cn.bmob.newim.notification.BmobNotificationManager;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
 
 /**
- * 这个页面显示的是一堆的瓶子，当然，它是以列表的形式展现的
+ * @author :smile
+ * @project:MainActivity
+ * @date :2016-01-15-18:23
  */
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends BaseActivity implements ObseverListener {
+
+    @Bind(R.id.btn_conversation)
+    Button btn_conversation;
+    @Bind(R.id.btn_set)
+    Button btn_set;
+
+    @Bind(R.id.btn_contact)
+    Button btn_contact;
+
+    @Bind(R.id.iv_conversation_tips)
+    ImageView iv_conversation_tips;
+
+    @Bind(R.id.iv_contact_tips)
+    ImageView iv_contact_tips;
+
+    private Button[] mTabs;
+    private ConversationFragment conversationFragment;
+    private SetFragment setFragment;
+    ContactFragment contactFragment;
+    private Fragment[] fragments;
+    private int index;
+    private int currentTabIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-        /**导航栏初始化*/
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        /**分页初始化*/
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        if (viewPager != null) {
-            setupViewPager(viewPager);
-        }
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        setContentView(R.layout.activity_main_test);
+        //connect server
+        User user = BmobUser.getCurrentUser(this,User.class);
+        BmobIM.connect(user.getObjectId(), new ConnectListener() {
+            @Override
+            public void done(String uid, BmobException e) {
+                if (e == null) {
+                    Logger.i("connect success");
+                    //服务器连接成功就发送一个更新事件，同步更新会话及主页的小红点
+                    EventBus.getDefault().post(new RefreshEvent());
+                } else {
+                    Logger.e(e.getErrorCode() + "/" + e.getMessage());
+                }
+            }
+        });
+        //监听连接状态，也可通过BmobIM.getInstance().getCurrentStatus()来获取当前的长连接状态
+        BmobIM.getInstance().setOnConnectStatusChangeListener(new ConnectStatusChangeListener() {
+            @Override
+            public void onChange(ConnectionStatus status) {
+                toast("" + status.getMsg());
+            }
+        });
+        //解决leancanary提示InputMethodManager内存泄露的问题
+        IMMLeaks.fixFocusedViewLeak(getApplication());
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void initView() {
+        super.initView();
+        mTabs = new Button[3];
+        mTabs[0] = btn_conversation;
+        mTabs[1] = btn_contact;
+        mTabs[2] =btn_set;
+        mTabs[0].setSelected(true);
+        initTab();
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+    private void initTab(){
+        conversationFragment = new ConversationFragment();
+        setFragment = new SetFragment();
+        contactFragment=new ContactFragment();
+        fragments = new Fragment[] {conversationFragment, contactFragment,setFragment};
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, conversationFragment).
+                add(R.id.fragment_container, contactFragment)
+                .add(R.id.fragment_container, setFragment)
+                .hide(setFragment).hide(contactFragment)
+                .show(conversationFragment).commit();
+    }
+
+    public void onTabSelect(View view) {
+        switch (view.getId()) {
+            case R.id.btn_conversation:
+                index = 0;
+                break;
+            case R.id.btn_contact:
+                index = 1;
+                break;
+            case R.id.btn_set:
+                index = 2;
+                break;
+        }
+        onTabIndex(index);
+    }
+
+    private void onTabIndex(int index){
+        if (currentTabIndex != index) {
+            FragmentTransaction trx = getSupportFragmentManager().beginTransaction();
+            trx.hide(fragments[currentTabIndex]);
+            if (!fragments[index].isAdded()) {
+                trx.add(R.id.fragment_container, fragments[index]);
+            }
+            trx.show(fragments[index]).commit();
+        }
+        mTabs[currentTabIndex].setSelected(false);
+        mTabs[index].setSelected(true);
+        currentTabIndex = index;
+    }
+
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        //显示小红点
+        checkRedPoint();
+        //进入应用后，通知栏应取消
+        BmobNotificationManager.getInstance(this).cancelNotification();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //清理导致内存泄露的资源
+        BmobIM.getInstance().clear();
+    }
 
-    static class Adapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragments = new ArrayList<>();
-        private final List<String> mFragmentTitles = new ArrayList<>();
+    /**注册消息接收事件
+     * @param event
+     */
+    @Subscribe
+    public void onEventMainThread(MessageEvent event){
+        checkRedPoint();
+    }
 
-        public Adapter(FragmentManager fm) {
-            super(fm);
+    /**注册离线消息接收事件
+     * @param event
+     */
+    @Subscribe
+    public void onEventMainThread(OfflineMessageEvent event){
+        checkRedPoint();
+    }
+
+    /**注册自定义消息接收事件
+     * @param event
+     */
+    @Subscribe
+    public void onEventMainThread(RefreshEvent event){
+        log("---主页接收到自定义消息---");
+        checkRedPoint();
+    }
+
+    private void checkRedPoint(){
+        int count = (int) BmobIM.getInstance().getAllUnReadCount();
+        if(count>0){
+            iv_conversation_tips.setVisibility(View.VISIBLE);
+        }else{
+            iv_conversation_tips.setVisibility(View.GONE);
         }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragments.add(fragment);
-            mFragmentTitles.add(title);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragments.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragments.size();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitles.get(position);
+        //是否有好友添加的请求
+        if(NewFriendManager.getInstance(this).hasNewFriendInvitation()){
+            iv_contact_tips.setVisibility(View.VISIBLE);
+        }else{
+            iv_contact_tips.setVisibility(View.GONE);
         }
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        Adapter adapter = new Adapter(getSupportFragmentManager());
-        adapter.addFragment(new CheeseListFragment(), "Category 1");
-        adapter.addFragment(new CheeseListFragment(), "Category 2");
-        adapter.addFragment(new CheeseListFragment(), "Category 3");
-        viewPager.setAdapter(adapter);
-    }
 }
