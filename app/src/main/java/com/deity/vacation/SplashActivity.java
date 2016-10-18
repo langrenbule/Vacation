c27edde20f202619dee2f4193d9881c304454f7c
langrenbule <546024423@qq.com>
2016-10-18 23:45:57 +0800

    (*)AD 开屏 (*)BaseRecycleView 带HeadView 或FootView

--- app/src/main/java/com/deity/vacation/SplashActivity.java
+++ app/src/main/java/com/deity/vacation/SplashActivity.java
@@ -3,16 +3,18 @@ package com.deity.vacation;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+import android.widget.RelativeLayout;
 
+import com.baidu.mobads.SplashAd;
+import com.baidu.mobads.SplashAdListener;
 import com.deity.vacation.base.BaseActivity;
 import com.deity.vacation.entity.User;
 import com.deity.vacation.entity.UserModel;
 
 
-/**启动界面
- * @author :smile
- * @project:SplashActivity
- * @date :2016-01-15-18:23
+/**
+ * 实时开屏，广告实时请求并且立即展现
  */
 public class SplashActivity extends BaseActivity {
 
@@ -20,6 +22,78 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // adUnitContainer
+        RelativeLayout adsParent = (RelativeLayout) this.findViewById(R.id.adsRl);
+
+        // the observer of AD
+        SplashAdListener listener = new SplashAdListener() {
+            @Override
+            public void onAdDismissed() {
+                Log.i("RSplashActivity", "onAdDismissed");
+                jumpWhenCanClick(); // 跳转至您的应用主界面
+            }
+
+            @Override
+            public void onAdFailed(String arg0) {
+                Log.i("RSplashActivity", "onAdFailed");
+                jump();
+            }
+
+            @Override
+            public void onAdPresent() {
+                Log.i("RSplashActivity", "onAdPresent");
+            }
+
+            @Override
+            public void onAdClick() {
+                Log.i("RSplashActivity", "onAdClick");
+                // 设置开屏可接受点击时，该回调可用
+            }
+        };
+        String adPlaceId = "2994371"; // 重要：请填上您的广告位ID，代码位错误会导致无法请求到广告
+        new SplashAd(this, adsParent, listener, adPlaceId, true);
+    }
+
+
+    /**
+     * 当设置开屏可点击时，需要等待跳转页面关闭后，再切换至您的主窗口。故此时需要增加canJumpImmediately判断。 另外，点击开屏还需要在onResume中调用jumpWhenCanClick接口。
+     */
+    public boolean canJumpImmediately = false;
+
+    private void jumpWhenCanClick() {
+        Log.d("test", "this.hasWindowFocus():" + this.hasWindowFocus());
+        if (canJumpImmediately) {
+//            this.startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            Handler handler =new Handler(Looper.getMainLooper());
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    User user = UserModel.getInstance().getCurrentUser();
+                    if (user == null) {
+                        startActivity(LoginActivity.class,null,true);
+                    }else{
+                        startActivity(MainActivity.class,null,true);
+                    }
+                }
+            },1000);
+            this.finish();
+        } else {
+            canJumpImmediately = true;
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        canJumpImmediately = false;
+    }
+
+    /**
+     * 不可点击的开屏，使用该jump方法，而不是用jumpWhenCanClick
+     */
+    private void jump() {
         Handler handler =new Handler(Looper.getMainLooper());
         handler.postDelayed(new Runnable() {
             @Override
@@ -32,6 +106,16 @@ public class SplashActivity extends BaseActivity {
                 }
             }
         },1000);
+//        this.startActivity(new Intent(RSplashActivity.this, MainActivity.class));
+        this.finish();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (canJumpImmediately) {
+            jumpWhenCanClick();
+        }
+        canJumpImmediately = true;
     }
 }
